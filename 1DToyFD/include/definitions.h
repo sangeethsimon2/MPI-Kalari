0d0d2e7d3bef66b5809bf97d01352e3103a1d057
sangeethsimon <sangeethsimon2@gmail.com>
2024-01-23 15:52:07 +0100

    Skeletal for MPI runs
    In this commit, we set up a skeletal project structure for
    MPI runs on this project.

---
+++ 1DToyFD/include/definitions.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+ #include <sstream>
+#include <cmath>
+#include <mpi.h>
+#include <cstddef>
+#include <algorithm>
+
+constexpr int MASTER=0;
+
+
+
